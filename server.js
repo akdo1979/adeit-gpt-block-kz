756c62d15f0cfc57a7b12e75efac723a4fc55f8f
akdo1979 <akdo70@yandex.ru>
2025-05-05 07:48:06 +0500

    Update server.js

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ app.post("/gpt", async (req, res) => {
 
   try {
     const chatCompletion = await groq.chat.completions.create({
-      model: "mistral-saba-24b",
+      model: "gemma2-9b-it",
       messages: sessionHistory
     });
 
